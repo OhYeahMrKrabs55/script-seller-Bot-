00b1009f891de6e99cf00404346ee936e320fe43
OhYeahMrKrabs55 <32842127+OhYeahMrKrabs55@users.noreply.github.com>
2017-10-16 09:38:55 -0400

    Create MyBot.js

---
+++ MyBot.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const bot = new Discord.Client ();
+
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+client.on('message', msg => {
+  if (msg.content === '!help') {
+    msg.reply('To buy these scripts read how-to-buy under Non-Buyer!');
+  }
+});
+client.on('message', msg => {
+	if (msg.content === '!test') {
+		msg.reply('Oh Ok');
+	}
+});
+client.login('MzY5MTgxMzQ1MTYwODg4MzMy.DMWikQ.IhkjVu84DznSxP43MUTCRMhxiFg');
